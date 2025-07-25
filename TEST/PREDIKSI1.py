5857d24e3887730eea302d47d0e803141b713bde
alfiansyah hidayat <105407023+alfiansyahhidayat@users.noreply.github.com>
2025-07-25 23:54:41 +0700

    Update PREDIKSI1.py

--- TEST/PREDIKSI1.py
+++ TEST/PREDIKSI1.py
@@ -28,7 +28,6 @@ def show():
     pakan = st.number_input("Pakan (gram)", min_value=0.0)
 
     input_data = pd.DataFrame({
-
         "Suhu (°C)": [suhu],
         "pH": [ph],
         "DO (mg/L)": [do],
@@ -39,19 +38,20 @@ def show():
     if st.button("Prediksi"):
         hasil = model.predict(input_data)[0]
         st.markdown(f"""
-    <div style="background-color:#1c4532; padding:20px; border-radius:10px; margin-top:20px">
-        <h3 style="color:#90EE90; text-align:center;">
-            ✅ Prediksi Produksi Bibit:<br> 
-            <span style="font-size:40px;">{int(hasil):,} ekor</span>
-        </h3>
-    </div>
-""", unsafe_allow_html=True)
+        <div style="background-color:#1c4532; padding:20px; border-radius:10px; margin-top:20px">
+            <h3 style="color:#90EE90; text-align:center;">
+                ✅ Prediksi Produksi Bibit:<br> 
+                <span style="font-size:40px;">{int(hasil):,} ekor</span>
+            </h3>
+        </div>
+        """, unsafe_allow_html=True)
 
     # Evaluasi jika dataset tersedia
     if 'dataset' in st.session_state:
         df = st.session_state['dataset']
 
-        expected_cols = ["Tahun","bulan","Minggu","Suhu (°C)", "pH", "DO (mg/L)", "Amonia (mg/L)", "Pakan", "Produksi Bibit (ekor)"]
+        # Hanya fitur yang digunakan model
+        expected_cols = ["Suhu (°C)", "pH", "DO (mg/L)", "Amonia (mg/L)", "Pakan", "Produksi Bibit (ekor)"]
         if not all(col in df.columns for col in expected_cols):
             st.warning(f"⚠️ Dataset tidak memiliki semua kolom: {expected_cols}")
             return
@@ -59,8 +59,6 @@ def show():
         X = df[["Suhu (°C)", "pH", "DO (mg/L)", "Amonia (mg/L)", "Pakan"]]
         y = df["Produksi Bibit (ekor)"]
         y_pred = model.predict(X)
-
-
 
         st.subheader("Perbandingan Nilai Aktual vs Prediksi")
         df_compare = df.copy()
